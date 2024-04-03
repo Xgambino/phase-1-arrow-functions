d8734298beedfdfd26836a08fca5aa3cd28dc6ae
Denis <mugambimuthuri0@gmail.com>
2024-04-03 09:41:51 +0300

    index.js

---
+++ index.js
@@ -0,0 +1,15 @@
+// helpers.js
+
+// Function expression called divide
+function divide(a, b) {
+   
+    return a / b;
+  }
+  
+  // Arrow function called square
+
+  const square = x => x * x;
+  
+  // Arrow function called add
+  
+  const add = (a, b) => a + b;
